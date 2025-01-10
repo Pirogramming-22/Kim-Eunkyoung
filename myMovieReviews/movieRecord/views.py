b80c21f9fa4841a1abb7d8879609a70df8e6511a
eunkyoung <eunkyoung4160@gmail.com>
2025-01-11 04:38:30 +0900

    readme 수정

--- myMovieReviews/movieRecord/views.py
+++ myMovieReviews/movieRecord/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import List
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -33,40 +34,10 @@ def review_list(request):
     context = {'lists' : lists}
     return render(request, 'reviews/list.html', context)
 
-# def review_update(request, pk):
-#     list = List.objects.get(id=pk)
-#     if request.method =="POST":
-#         title2 = request.POST.get('title')
-#         year2 = request.POST.get('year')
-#         genre2 = request.POST.get('genre')
-#         rating2 = request.POST.get('rating')
-#         runtime2 = request.POST.get('runtime')
-#         review2 = request.POST.get('review')
-#         director2 = request.POST.get('director')
-#         actors2 = request.POST.get('actors')
-#         list.title = title2
-#         list.year = year2
-#         list.genre = genre2
-#         list.rating = rating2
-#         list.runtime = runtime2
-#         list.review = review2
-#         list.director = director2
-#         list.actors = actors2
-#         if not list.title:
-#             list.title = 'Untitled'
-#         list.save()
-#         return redirect('movieRecord:review_detail', pk=list.pk)
-    
-#     context = {
-#         'list':list
-#     }
-#     return render(request, 'board/update.html', context)
-
 def review_update(request, pk):
     list = List.objects.get(id=pk)
     if request.method == "POST":
-        # 기존 업데이트 코드
-        list.title = request.POST.get('title', 'Untitled')
+        list.title = request.POST.get('title')
         list.year = request.POST.get('year')
         list.genre = request.POST.get('genre')
         list.rating = request.POST.get('rating')
@@ -76,10 +47,11 @@ def review_update(request, pk):
         list.actors = request.POST.get('actors')
         list.save()
         return redirect('movieRecord:review_detail', pk=list.pk)
-
-    # 수정 페이지에 데이터 전달
-    context = {'list': list}
-    return render(request, 'board/update.html', context)
+    
+    context = {
+        'list': list
+    }
+    return render(request, 'reviews/update.html', context)
 
 
 def review_delete(request, pk):
